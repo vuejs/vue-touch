2995ce96ba48a9aa903b142eed90455830390bd0
Luke Boyle <luke.boyle.au@gmail.com>
2015-12-17 20:51:04 +1100

    refactor: imports dependencies

---
+++ vue-touch.js
@@ -0,0 +1,6 @@
+var Hammer = require('hammerjs');
+var Vue = require('vue');
+var isNotSet = require('is-not-set');
+var allege = require('allege');
+var arrayUnique = require('array-uniq');
+var isFunction = require('is-function');
