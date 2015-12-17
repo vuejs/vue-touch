c7b6f8e2eae1f22eaf73a374fff89d069c2c721e
Luke Boyle <luke.boyle.au@gmail.com>
2015-12-17 20:58:05 +1100

    chore: update examples to latest build

---
+++ example/script.js
@@ -0,0 +1,18 @@
+var Vue = require('vue');
+var vueTouch = require('../vue-touch.js');
+
+vueTouch.init([
+    'swipe-left'
+]);
+
+function callback() {
+    alert('hello');
+}
+
+global.pageView = new Vue ({
+    el: 'div',
+    methods: {
+        callback: callback
+    }
+});
+
