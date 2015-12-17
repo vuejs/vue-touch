ad4c1abea24535bc7d63f23bef94468bdf28cb46
Luke Boyle <luke.boyle.au@gmail.com>
2015-12-17 20:54:27 +1100

    refactor: ensures data integrity of input before taking action

--- vue-touch.js
+++ vue-touch.js
@@ -10,10 +10,43 @@ var supportedDirectives = [
 ];
 
 /**
+/**
+ *
+ * @param {Array.<*>} arrayInput - the array to compare
+ * @returns {boolean}
+ */
+function areAllArrayItemsUnique(arrayInput) {
+	var uniqueArrayLength = arrayUnique(arrayInput).length;
+	var rawArrayLength = arrayInput.length;
+	return uniqueArrayLength === rawArrayLength;
+}
+
+/**
  *
  * @param {Array.<String>} directivesToEnable - The directives you wish to enable.
  */
 function init(directivesToEnable) {
+	if (isNotSet(directivesToEnable)) {
+		throw new ReferenceError('Required argument "directivesToEnable" was not set.');
+	}
+	if (Array.isArray(directivesToEnable) === false) {
+		throw new TypeError('Argument "directivesToEnable" was not of required type Array');
+	}
+	if (directivesToEnable.length === 0) {
+		throw new RangeError('"directivesToEnable" was passed an empty array');
+	}
+	directivesToEnable.forEach(function(directive) {
+		if (allege(directive).isNoneOf.apply(this, supportedDirectives)) {
+			throw new RangeError(
+				directive + ' is not a supported directive. Supported directives are ' + supportedDirectives
+			)
+		}
+	});
+	if (areAllArrayItemsUnique(directivesToEnable) === false) {
+		console.warn(
+			'You are trying to enable directives multiple times in "directivesToEnable". Please make directives unique'
+		);
+	}
 }
 
 module.exports = {
