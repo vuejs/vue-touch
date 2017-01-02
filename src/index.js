4a2c7ac9373acc0e16ceab829f7a3cd1eb9c5745
Thorsten <t.luenborg@googlemail.com>
2017-01-02 13:44:38 +0100

    moving util functions to separate module.

--- src/index.js
+++ src/index.js
@@ -1,47 +1,17 @@
-const assign = require('object.assign').getPolyfill()
+import assignPolyfill from 'object.assign'
+import {
+  createProp,
+  capitalize,
+  guardDirections,
+  gestures,
+  gestureMap,
+  directions
+} from './utils'
+
+const assign = assignPolyfill.getPolyfill()
 
 let Hammer
 
-const gestures = [
-  'pan','panstart','panmove','panend','pancancel','panleft','panright','panup','pandown',
-  'pinch','pinchstart','pinchmove','pinchend','pinchcancel','pinchin','pinchout',
-  'press','pressup',
-  'rotate','rotatestart','rotatemove','rotateend','rotatecancel',
-  'swipe','swipeleft','swiperight','swipeup','swipedown',
-  'tap'
-]
-const gestureMap = {
-  pan: 'pan',
-  panstart: 'pan',
-  panmove: 'pan',
-  panend: 'pan',
-  pancancel: 'pan',
-  panleft: 'pan',
-  panright: 'pan',
-  panup: 'pan',
-  pandown: 'pan',
-  pinch: 'pinch',
-  pinchstart: 'pinch',
-  pinchmove: 'pinch',
-  pinchend: 'pinch',
-  pinchcancel: 'pinch',
-  pinchin: 'pinch',
-  pinchout: 'pinch',
-  press: 'press',
-  pressup: 'press',
-  rotate: 'rotate',
-  rotatestart: 'rotate',
-  rotatemove: 'rotate',
-  rotateend: 'rotate',
-  rotatecancel: 'rotate',
-  swipe: 'swipe',
-  swipeleft: 'swipe',
-  swiperight: 'swipe',
-  swipeup: 'swipe',
-  swipedown: 'swipe',
-  tap: 'tap'
-}
-const directions = ['up', 'down', 'left', 'right', 'horizontal', 'vertical', 'all']
 const customEvents = {}
 let installed = false
 
@@ -188,29 +158,6 @@ vueTouch.registerCustomEvent = function registerCustomEvent(event, options = {})
 // Utilities
 // ********
 
-function createProp() {
-  return {
-    type: Object,
-    default: function() { return {} }
-  }
-}
-
-function capitalize (str) {
-  return str.charAt(0).toUpperCase() + str.slice(1)
-}
-
-function guardDirections (options) {
-  var dir = options.direction
-  if (typeof dir === 'string') {
-    var hammerDirection = 'DIRECTION_' + dir.toUpperCase()
-    if (directions.indexOf(dir) > -1 && Hammer.hasOwnProperty(hammerDirection)) {
-      options.direction = Hammer[hammerDirection]
-    } else {
-      console.warn('[vue-touch] invalid direction: ' + dir)
-    }
-  }
-  return options
-}
 
 if (typeof exports == "object") {
   module.exports = vueTouch
---
+++ src/utils.js
@@ -0,0 +1,64 @@
+export function createProp() {
+  return {
+    type: Object,
+    default: function() { return {} }
+  }
+}
+
+export function capitalize (str) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+export function guardDirections (options) {
+  var dir = options.direction
+  if (typeof dir === 'string') {
+    var hammerDirection = 'DIRECTION_' + dir.toUpperCase()
+    if (directions.indexOf(dir) > -1 && Hammer.hasOwnProperty(hammerDirection)) {
+      options.direction = Hammer[hammerDirection]
+    } else {
+      console.warn('[vue-touch] invalid direction: ' + dir)
+    }
+  }
+  return options
+}
+
+export const gestures = [
+  'pan','panstart','panmove','panend','pancancel','panleft','panright','panup','pandown',
+  'pinch','pinchstart','pinchmove','pinchend','pinchcancel','pinchin','pinchout',
+  'press','pressup',
+  'rotate','rotatestart','rotatemove','rotateend','rotatecancel',
+  'swipe','swipeleft','swiperight','swipeup','swipedown',
+  'tap'
+]
+export const gestureMap = {
+  pan: 'pan',
+  panstart: 'pan',
+  panmove: 'pan',
+  panend: 'pan',
+  pancancel: 'pan',
+  panleft: 'pan',
+  panright: 'pan',
+  panup: 'pan',
+  pandown: 'pan',
+  pinch: 'pinch',
+  pinchstart: 'pinch',
+  pinchmove: 'pinch',
+  pinchend: 'pinch',
+  pinchcancel: 'pinch',
+  pinchin: 'pinch',
+  pinchout: 'pinch',
+  press: 'press',
+  pressup: 'press',
+  rotate: 'rotate',
+  rotatestart: 'rotate',
+  rotatemove: 'rotate',
+  rotateend: 'rotate',
+  rotatecancel: 'rotate',
+  swipe: 'swipe',
+  swipeleft: 'swipe',
+  swiperight: 'swipe',
+  swipeup: 'swipe',
+  swipedown: 'swipe',
+  tap: 'tap'
+}
+export const directions = ['up', 'down', 'left', 'right', 'horizontal', 'vertical', 'all']
