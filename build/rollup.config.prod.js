244dc3cef60b94c2346033b1d9bc01a5e39a1856
Thorsten <t.luenborg@googlemail.com>
2017-01-04 14:33:20 +0100

    we want to support including npm_modules in the bundle, but also explore hammer for now.

--- build/rollup.config.prod.js
+++ build/rollup.config.prod.js
@@ -1,8 +1,15 @@
 import buble from 'rollup-plugin-buble'
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
 
 export default {
   entry: './src/index.js',
   format: 'umd',
+  external: ['hammer'],
   dest: './dist/vue-touch.js', // equivalent to --output
-  plugins: [buble()]
-};
+  plugins: [
+    buble(),
+    nodeResolve({ jsnext: true, main: true }),
+    commonjs(),
+  ]
+}
