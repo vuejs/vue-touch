e0cfdcf15b398e24209750bfaeed8459bfe54034
Thorsten <t.luenborg@googlemail.com>
2017-01-14 14:07:05 +0100

    finished tests for enabled prop.

--- test/unit/enabledProp.js
+++ test/unit/enabledProp.js
@@ -14,20 +14,10 @@ import {
 
 let vt
 
-test.skip('before', t => {
-
-  vt = createFromTemplate(`
-    <v-touch
-    @tap="cb"
-    @swipe="cb"
-    />
-  `)
-})
-
 test('prop is true by default & events are enabled', t => {
   t.plan(2)
 
-  vt = createFromTemplate(`
+  const vt = createFromTemplate(`
     <v-touch
     @tap="cb"
     @swipe="cb"
@@ -36,10 +26,54 @@ test('prop is true by default & events are enabled', t => {
   const propEnabled = vt.enabled
   t.true(propEnabled)
 
-  const rcEnabled = isEnabled(vm, 'tap')
+  const rcEnabled = isEnabled(vt, 'tap')
   t.true(rcEnabled)
 })
 
-test.todo('all rcg disabled when enabled="false"')
+test('all rcg disabled when enabled="false"', t => {
+  t.plan(2)
 
-test.todo('Passing obj to enabled prop correctly toggles recognizers')
+  const vt = createFromTemplate(`
+    <v-touch
+    v-bind:enabled="false"
+    v-on:tap="cb"
+    v-on:swipe="cb"
+    />
+  `)
+
+  t.false(vt.enabled)
+  const areAllDisabled = allDisabled(vt)
+  t.true(areAllDisabled)
+})
+
+test.cb('Passing obj to enabled prop correctly toggles recognizers', t => {
+  t.plan(2)
+
+  const vt = createFromTemplate(`
+    <v-touch
+    v-bind:enabled="{ tap: true, swipe: false }"
+    v-on:tap="cb"
+    v-on:swipe="cb"
+    />
+  `)
+
+  vt.$nextTick()
+    .then(() => {
+      const tapEnabled = isEnabled(vt, 'tap')
+      const swipeDisabled =  isDisabled(vt, 'swipe')
+      t.true(tapEnabled && swipeDisabled)
+    })
+
+    .then(() => {
+      vt.updateEnabled({ tap: false, swipe: true })
+
+      return vt.$nextTick()
+    })
+
+    .then(() => {
+      const tapDisabled = isDisabled(vt, 'tap')
+      const swipeEnabled= isEnabled(vt, 'swipe')
+      t.true(tapDisabled && swipeEnabled)
+      t.end()
+    })
+})
