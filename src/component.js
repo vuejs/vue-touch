04832dfbec74acab5d08e555fcd505b117c6964e
Thorsten <t.luenborg@googlemail.com>
2017-01-16 19:37:22 +0100

    FEATURE: Set general Hammer options with the 'options' prop.

--- src/component.js
+++ src/component.js
@@ -14,6 +14,7 @@ import {
 
 export default {
   props: {
+    options: createProp(),
     tapOptions: createProp(),
     panOptions: createProp(),
     pinchOptions: createProp(),
@@ -30,7 +31,7 @@ export default {
 
   mounted() {
     if (!this.$isServer) {
-      this.hammer = new Hammer.Manager(this.$el)
+      this.hammer = new Hammer.Manager(this.$el, this.options)
       this.recognizers = {} // not reactive
       this.setupBuiltinRecognizers()
       this.setupCustomRecognizers()
