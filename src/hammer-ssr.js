809eb99efb201c1d277e7413c34c90ec349e6fbb
Nguyen Quoc Anh <quocanh96@gmail.com>
2017-06-30 00:06:25 +0700

    fix: syntax (#87)

--- src/hammer-ssr.js
+++ src/hammer-ssr.js
@@ -1,4 +1,4 @@
-module.exports = function Hammer {
+module.exports = function Hammer() {
   console.log(`[vue-touch] Your should never see this message.
     When you do, your code tried to call 'new Hammer(), but your app has included a stub for HammerJS, provided by vue-touch, instead of the actual HammerJS library.
   `)
